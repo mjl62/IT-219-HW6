9405fde21b37381f25b6fdb3e702880b7ff8193a
mjl62 <mjl62@njit.edu>
2024-02-27 14:31:29 -0500

    Added multiprocessing to commands

--- app/plugins/exit/__init__.py
+++ app/plugins/exit/__init__.py
@@ -1,10 +1,14 @@
 from app.command import Command
 from typing import List
+import os
+import signal
+from multiprocessing import Queue
 
 class ExitCommand(Command):
     
-    def execute(args: List):
-        exit()
+    def execute(args: List, queue: Queue):
+        # exit main program thread
+        queue.put("EXITCODE_0")
 
     def menuText() -> str:
         return "Exit the application"
